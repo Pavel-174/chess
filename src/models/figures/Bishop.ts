b850af25805c1f65ef06351640a4790c7228e9c9
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-02-03 17:42:32 +0500

    логика пермещения фигур готова

--- src/models/figures/Bishop.ts
+++ src/models/figures/Bishop.ts
@@ -7,16 +7,16 @@ import whiteLogo from "../../assets/white-bishop.png"
 export class Bishop extends Figure {
 
     constructor(color: Colors, cell: Cell) {
-        super(color, cell);
-        this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
-        this.name = FigureNames.BISHOP;
+      super(color, cell);
+      this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
+      this.name = FigureNames.BISHOP;
     }
-
+  
     canMove(target: Cell): boolean {
-        if(!super.canMove(target))
-          return false;
-        if(this.cell.isEmptyDiagonal(target))
-          return true
-        return false
+      if(!super.canMove(target))
+        return false;
+      if(this.cell.isEmptyDiagonal(target))
+        return true
+      return false
     }
 }
